0dd183b0e6834b0af35214900a59023a51b55243
Kyle 🐆 <kyle@privkey.io>
2026-05-25 21:43:37 -0400

    Version 0.4.0:0 (mirror keep 0.4.0); bump submodule

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v0_3_0_0 } from './v0.3.0_0'
+import { v0_4_0_0 } from './v0.4.0_0'
 
 export const versionGraph = VersionGraph.of({
-  current: v0_3_0_0,
+  current: v0_4_0_0,
   other: [],
 })
--- startos/versions/v0.4.0_0.ts
+++ startos/versions/v0.4.0_0.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
-export const v0_3_0_0 = VersionInfo.of({
-  version: '0.3.0:0',
+export const v0_4_0_0 = VersionInfo.of({
+  version: '0.4.0:0',
   releaseNotes: {
     en_US:
       'Initial release of Keep, the always-on FROST co-signer, for StartOS.',
