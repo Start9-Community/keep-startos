285b31ed9ba94ce5fd2a244d90df92b7a981c928
Kyle 🐆 <kyle@privkey.io>
2026-05-27 14:18:47 -0400

    Bump keep to v0.4.1: peer presence in activity feed (0.4.1:0)

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,8 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { v0_4_0_0 } from './v0.4.0_0'
+import { v0_4_1_0 } from './v0.4.1_0'
 
 export const versionGraph = VersionGraph.of({
-  current: v0_4_0_0,
-  other: [],
+  current: v0_4_1_0,
+  other: [v0_4_0_0],
 })
