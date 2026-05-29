105629a89b1c40824e7def3be9c971d687fe60df
Kyle 🐆 <kyle@privkey.io>
2026-05-29 19:40:08 -0400

    Bump keep to v0.4.7: bounded multi-event pre-approval + single-party FROST sign/ECDH

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -5,8 +5,9 @@ import { v0_4_2_0 } from './v0.4.2_0'
 import { v0_4_3_0 } from './v0.4.3_0'
 import { v0_4_5_0 } from './v0.4.5_0'
 import { v0_4_6_0 } from './v0.4.6_0'
+import { v0_4_7_0 } from './v0.4.7_0'
 
 export const versionGraph = VersionGraph.of({
-  current: v0_4_6_0,
-  other: [v0_4_0_0, v0_4_1_0, v0_4_2_0, v0_4_3_0, v0_4_5_0],
+  current: v0_4_7_0,
+  other: [v0_4_0_0, v0_4_1_0, v0_4_2_0, v0_4_3_0, v0_4_5_0, v0_4_6_0],
 })
